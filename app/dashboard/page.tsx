95c9bac91906461d358cdbf09097932985778775
Akem-Ben <akemini.ndaobong@gmail.com>
2025-03-18 01:09:36 +0100

    Navbar completion

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,22 @@
+"use client";
 import React from 'react'
 import Sidebar from '@/components/Sidebar';
+import Navbar from '@/components/Navbar';
+import { useMatchMediaQuery } from "@/hooks/useViewPort";
+import device from "@/constants/breakpoints";
 
 const Dashboard = () => {
+  const isTabletOrSmaller = useMatchMediaQuery(device.tablet);
+
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Sidebar />
+      <div className={`flex-1 ${isTabletOrSmaller ? 'ml-0' : 'ml-[179px]'} transition-all duration-300`}>
+        <Navbar />
+        
+        <div className="p-4 sm:p-6 md:p-8 bg-[#fafafe] min-h-screen">
+        </div>
+      </div>
     </div>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavbarNotificationBell, NavbarSettingsIcon } from "@/layout/svgIconPaths";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white text-[#785DBA] text-[22px] leading-[145%] font-[700] px-4 sm:px-6 md:px-8 lg:px-20 h-[76px] border-b border-[#66666659] shadow-lg flex justify-between items-center w-full">
+      <div>
+        Hello Tunji
+      </div>
+      <div className="gap-[24px] items-center justify-between flex">
+        <nav className="hover:cursor-pointer">
+          <NavbarNotificationBell />
+        </nav>
+        <nav className="hover:cursor-pointer">
+          <NavbarSettingsIcon />
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
