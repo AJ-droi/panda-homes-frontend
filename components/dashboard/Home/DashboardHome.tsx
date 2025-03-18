1d17a4e0bfdc1f9c15b5af75e2819c2bfad8c549
Akem-Ben <akemini.ndaobong@gmail.com>
2025-03-18 02:37:30 +0100

    Property Payment Table Completion

--- components/dashboard/Home/DashboardHome.tsx
+++ components/dashboard/Home/DashboardHome.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from 'react'
 import DashboardHomeNav from './Homenav'
+import PropertyPaymentTable from './PropertyPaymentTable';
 
 const DashboardHome = () => {
   return (
     <div>
+        <section>
         <div className='text-[#4D4D4D] font-[600] text-[22px] leading-[145%]'
         style={{fontFamily: 'Plus Jakarta Sans'}}
         >
@@ -14,6 +16,18 @@ const DashboardHome = () => {
         <div className='mt-6'>
       <DashboardHomeNav />
         </div>
+        </section>
+
+    <section>
+        <div className='text-[#4D4D4D] font-[600] mt-6 text-[22px] leading-[145%]'
+        style={{fontFamily: 'Plus Jakarta Sans'}}
+        >
+        Upcoming Rent Payments
+        </div>
+        <div className='mt-6 flex items-start justify-items-start'>
+            <PropertyPaymentTable />
+        </div>
+    </section>
     </div>
   )
 }
