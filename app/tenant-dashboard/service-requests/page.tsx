3dfce9d67516ae515c5582f51811b1ecc63e4d41
AJ-droi <ajiriosiobe@gmail.com>
2025-06-05 15:50:47 +0100

    tenant dashboard

--- app/tenant-dashboard/service-requests/page.tsx
+++ app/tenant-dashboard/service-requests/page.tsx
@@ -1,14 +1,15 @@
 "use client";
-import TenantServiceRequestHome from "@/components/tenantsSection/tenantServiceRequest/TenantServiceRequestHome";
+import CustomerServiceChat from "@/components/tenantsSection/tenantServiceRequest/TenantServiceChat";
 import React from "react";
 
 const TenantServiceRequests = () => {
 
   return (
-    <div className="flex flex-col min-h-screen">
-        <div className="p-4 sm:p-6 md:p-8 bg-[#fafafe] min-h-screen">
-          <TenantServiceRequestHome />
-        </div>
+    <div className="flex flex-col min-h-screen bg-[#fff] w-full">
+
+
+          <CustomerServiceChat />
+      
       </div>
   );
 };
