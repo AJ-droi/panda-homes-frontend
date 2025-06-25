4b749ee7039138e8c5c300fc667eec59f010f7f9
AJ-droi <ajiriosiobe@gmail.com>
2025-06-25 08:40:38 +0100

    no data

--- components/tenantsSection/tenantServiceRequest/OngoingRequest.tsx
+++ components/tenantsSection/tenantServiceRequest/OngoingRequest.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import RequestCard from './RequestCard'
 import { useFetchTenantServiceRequest } from '@/services/tenants/query';
+import NoDataAvailable from '../NoDataComponent';
 
 const OngoingRequest = () => {
 
@@ -13,7 +14,17 @@ const OngoingRequest = () => {
   return (
      <div className='bg-[#fff] min-h-[100vh] text-[#000] py-[2%] px-[3%] '>
         <h3 className='text-[#170F49] font-bold text-xl py-[2%]'>Ongoing Requests </h3>
-        <RequestCard request={tenantServiceRequest} type={'ongoing'}/>
+        
+      {tenantServiceRequest?.length === 0 ? (
+        <NoDataAvailable
+          title="No Ongoing Request Available"
+          subtitle=""
+          description=""
+          footer=""
+        />
+      ) : (<RequestCard request={tenantServiceRequest} type={'ongoing'}/>
+        
+      )}
     </div>
   )
 }
--- components/tenantsSection/tenantServiceRequest/PastRequest.tsx
+++ components/tenantsSection/tenantServiceRequest/PastRequest.tsx
@@ -1,25 +1,31 @@
-    "use client"
-import React from 'react'
-import RequestCard from './RequestCard'
-import { useFetchTenantServiceRequest } from '@/services/tenants/query';
+"use client";
+import React from "react";
+import RequestCard from "./RequestCard";
+import { useFetchTenantServiceRequest } from "@/services/tenants/query";
+import NoDataAvailable from "../NoDataComponent";
 
 const PastRequest = () => {
+  const { data: tenantServiceRequest } =
+    useFetchTenantServiceRequest("resolved");
 
-
-    const {
-          data: tenantServiceRequest,
-        } = useFetchTenantServiceRequest(
-         'resolved'
-        );
   return (
-     <div className='bg-[#fff] min-h-[100vh] text-[#000] py-[2%] px-[3%] '>
-        <h3 className='text-[#170F49] font-bold text-xl py-[2%]'>Past Requests </h3>
-        <RequestCard request={tenantServiceRequest} type={'past'} />
+    <div className="bg-[#fff] min-h-[100vh] text-[#000] py-[2%] px-[3%] ">
+      <h3 className="text-[#170F49] font-bold text-xl py-[2%]">
+        Past Requests{" "}
+      </h3>
 
-  
+      {tenantServiceRequest?.length === 0 ? (
+        <NoDataAvailable
+          title="No Past Request Available"
+          subtitle=""
+          description=""
+          footer=""
+        />
+      ) : (
+        <RequestCard request={tenantServiceRequest} type={"past"} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-
-export default PastRequest
+export default PastRequest;
